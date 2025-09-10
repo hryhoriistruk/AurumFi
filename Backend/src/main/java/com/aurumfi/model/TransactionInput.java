7b3b0168ea1d45294276b550a5baa6d2519add44
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2025-09-11 01:11:01 +0200

    Delete Backend directory

--- Backend/src/main/java/com/aurumfi/model/TransactionInput.java
+++
@@ -1,9 +0,0 @@
-package com.aurumfi.model;
-
-public class TransactionInput {
-    public String transactionOutputId;
-
-    public TransactionInput(String transactionOutputId) {
-        this.transactionOutputId = transactionOutputId;
-    }
-}
